361112d541e51e261a40205d5c1fceacbdf00ba7
Abhishek Mahato <abhishekmahato2002@gmail.com>
2020-09-26 11:53:22 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express           = require("express"),
       bodyParser        = require("body-parser"),
       mongoose          = require("mongoose"),
+      flash             = require("connect-flash"),
       passport          = require("passport"),
       LocalStrategy     = require("passport-local"),
+      methodOverride    = require("method-override"),
       Campground        = require("./models/campground"),
       Comment           = require("./models/comment"),
       User              = require("./models/user"),
       seedDB            = require("./seeds"),
-      app               = express(),
-      PORT              = 3000;
+      app               = express();
+
+const PORT = process.env.PORT || 3000;
 
 //Requiring Routes
 const commentRoutes     = require("./routes/comments"),
       campgroundRoutes  = require("./routes/campgrounds"),
       indexRoutes        = require("./routes/index");
 
-mongoose.connect('mongodb://localhost/abhi_camp_v9',{useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect('mongo "mongodb+srv://abhicamp-wqzeq.mongodb.net/abhicamp" --username foreverabhi2002',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() =>{
+    console.log("Connected to DB!");
+}).catch(err => {
+    console.log("ERROR:",err.message);
+});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
 //seedDB(); //Seed the database
 
 // PASSPORT CONFIGURATION
@@ -38,6 +50,8 @@ passport.deserializeUser(User.deserializeUser());
 app.use(function(req,res,next)
 {
     res.locals.currentUser=req.user;
+    res.locals.error=req.flash("error");
+    res.locals.success=req.flash("success");
     next();
 });
 
@@ -45,7 +59,6 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(PORT,function(req,res)
-{
-    console.log("Abhicamp started at PORT 3000");
+app.listen(PORT, () => {
+    console.log(`Abhicamp started at PORT ${ PORT }`);
 });
